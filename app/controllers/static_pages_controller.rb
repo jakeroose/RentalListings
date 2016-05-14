3bd1dcc10e12edae7961751e95e8201a7f38003e
Jake <jakeroose.b@gmail.com>
2016-05-13 23:42:01 -0400

    Added static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def about
+  end
+
+  def policies
+  end
+
+  def utilities
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :listings
   root 'listings#index'
+
+  get '/about', to: 'static_pages#about'
+  get '/policies', to: 'static_pages#policies'
+  get '/utilities', to: 'static_pages#utilities'
+  get '/contact', to: 'static_pages#contact'
 end
