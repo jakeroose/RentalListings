a45ae6c52142cff01d57b43c888090529126d96f
Jake <jakeroose.b@gmail.com>
2016-06-01 09:39:30 -0400

    Cleaned up a bit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
     @user = User.find(params[:id])
+    @favorite_listings = @user.favorite_listings.page(params[:page])
   end
 end
